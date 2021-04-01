1585f5b7001ccd27eadd7a2c12ab690a0e081110
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-03-31 21:24:13 -0700

    refactor Player to take in potion

---
+++ __tests__/Player.test.js
@@ -0,0 +1,20 @@
+const { TestScheduler } = require("jest");
+const Player = require("../lib/Player");
+
+// imports Potion() constructor into test, est Potion as variable
+const Potion = require("../lib/Potion");
+
+jest.mock("../lib/Potion");
+console.log(new Potion());
+
+test("creates a player object", () => {
+    const player = new Player("Dave");
+
+    expect(player.name).toBe("Dave");
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
